e5e5a7a28e8c845fe0c6ddc95d91f0976a648c09
Wilson <wilsontancher@gmail.com>
2024-05-14 21:06:33 +0800

    Ex C - Step 2A
    Rectify error in javascript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 setInterval(function () {
     var now = new Date();
-    $("#realtimeclockj").html(now.toDateString() + "<br>" + now.toTimeString());
+    $("#realtimeclock").html(now.toDateString() + "<br>" + now.toTimeString());
 }, 500);
