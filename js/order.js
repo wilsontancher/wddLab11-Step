1f80ce56766c46ed1740378187b8156308e41d69
Wilson <wilsontancher@gmail.com>
2024-05-14 17:40:45 +0800

    Ex A - Step 5
    First part of ComputeTotal function:
    Grab values from the web page elements and store them into arrays.

--- js/order.js
+++ js/order.js
@@ -4,3 +4,23 @@ $(function () {
         computeTotal();
     });
 });
+
+function computeTotal() {
+    /* Grab values from elements and store in array for further processing */
+    var itemNameArray = []; // initialise empty array for item names
+    var priceArray = []; // initialise empty array for prices
+    var qtyArray = []; // initialise empty array for qty
+    var selectedQty = 0; // used to test if any qty>0 has been selected
+    $(".itemname").each(function () { // we grab the values of each of the item's name from the itemname class into an array
+        itemNameArray.push($(this).text());
+    });
+    $(".price").each(function () { // we grab the values of each of the item's price from the price class into an array
+        priceArray.push(parseFloat($(this).text()));
+    });
+    $(".selectitem option:selected").each(function () { // we grab the values of each of the item selected qty from the selectitem class into an array
+        qtyArray.push(parseFloat($(this).val()));
+        selectedQty += parseFloat($(this).val()); // increase qty selected
+    });
+
+    
+}
